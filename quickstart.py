aee9429b4f341c3e6536f2a07f99aa451bbe239f
Pankaj singh rajput <58437823+psr672000@users.noreply.github.com>
2020-09-13 12:54:09 +0530

    Update quickstart.py

--- quickstart.py
+++ quickstart.py
@@ -9,12 +9,11 @@ from instapy import set_workspace
 # set workspace folder at desired location (default is at your home folder)
 set_workspace(path=None)
 
+headless_browser=True
+
 # get an InstaPy session!
 session = InstaPy()
 
 with smart_run(session):
-    # general settings
-    session.set_dont_include(["friend1", "friend2", "friend3"])
-
     # activity
     session.like_by_tags(["natgeo"], amount=10)
